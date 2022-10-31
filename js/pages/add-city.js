6530b991c528e90e974938d1d0f788b206854315
sklucho <perg911@gmail.com>
2022-10-31 04:25:37 -0300

    addcity terminado

--- js/pages/add-city.js
+++ js/pages/add-city.js
@@ -1,9 +1,13 @@
+//guardando el elemento button en una variable
 submit = document.querySelector('#button');
 
+//declarando un evento con función de flecha
 submit.addEventListener("click", (e) => {
 
+    //deteniendo el accionar por defecto del formulario
     e.preventDefault();
 
+    //funciones
     function getCitiesFromLocalStorage() {
         let cities = localStorage.getItem("CITIES");
         if (cities) {
@@ -17,17 +21,26 @@ submit.addEventListener("click", (e) => {
     function addNewCityToLocalStorage() {
         let cities = getCitiesFromLocalStorage();
         let newCity = document.querySelector('#addcity').value;
-        newCity.toLowerCase();
+        let indice = cities.indexOf(newCity);
 
-        //Recorrer el array para validar que exista ciudad
         
+        //validaciones
+        if (indice !== -1) {
+            document.querySelector('#yellow').style.display = 'block';
+        } else if (newCity == 0 || !isNaN(newCity)) {
+            document.querySelector('#red').style.display = 'block';
+        } else {
+            document.querySelector('#green').style.display = 'block';
+            cities.push(newCity);
+        }
 
-        cities.push(newCity);
         localStorage.setItem("CITIES", JSON.stringify(cities));
     }
 
+    //iniciando la función
     addNewCityToLocalStorage();
 
+    //definiendo el tiempo en el que van a aparecer los carteles de colores
     setTimeout(() => {
         window.location.reload();
     }, 3000);
